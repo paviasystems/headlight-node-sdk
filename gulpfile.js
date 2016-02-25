e0ab2ce2708a7a61b03384a1c82ce795fd17352a
Steven Velozo <steven@velozo.com>
2016-02-25 13:22:55 -0800

    Swill file

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+/**
+* This file is part of the Pavia Headlight Apps SDK
+*
+* @license     All Rights Reserved
+* @copyright   Copyright (c) 2016, Pavia
+*
+* @author      Steven Velozo <steven@paviasystems.com>
+*
+* @description This is the swill build file to test the javascript, css, html and less.  Plus generate / minify the site.
+*/
+var _Swill = require(__dirname+'/server/Headlight-NodeSDK.js').new({StaticContentFolder: __dirname+'/stage/'}).swill();
