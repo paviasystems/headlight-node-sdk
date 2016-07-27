80ffb5dbff545d43c2a1c507b76e0639019e517a
Steven Velozo <steven@velozo.com>
2016-07-27 22:25:46 +0000

    Adding app template

---
+++ rock/gulpfile.js
@@ -0,0 +1,17 @@
+/**
+* This includes the Pavia Headlight Node SDK gulpfile
+*
+* @license     All Rights Reserved
+* @copyright   Copyright (c) 2016, Pavia
+*
+* @author      Ryan Vanderpol <me@ryanvanderpol.com>
+*
+* @description This is the swill build file to test the javascript, css, html and less.  Plus generate / minify the site.
+*/
+
+var _Swill = require('headlight-node-sdk').new({StaticContentFolder: __dirname+'/stage/'}).swill();
+
+// watch for changes
+_Swill.gulp.task('watch', function() {
+    _Swill.gulp.watch(['headlight-app/**/*'],['build-debug']);
+});
